9f76e44c0762fe9f8b353d33386389fc9ddeab08
Cucloid37 <81418959+Cucloid37@users.noreply.github.com>
2021-05-14 19:57:39 +0300

    Loading HW_1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSpeed = -20f;
+    public float speed = 5.0f;
+
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    void Start ()
+    {
+        m_Animator = GetComponent<Animator> ();
+        m_Rigidbody = GetComponent<Rigidbody> ();
+        m_AudioSource = GetComponent<AudioSource> ();
+    }
+
+    void FixedUpdate ()
+    {
+        float horizontal = Input.GetAxis ("Horizontal");
+        float vertical = Input.GetAxis ("Vertical");
+        
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize ();
+
+        bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool ("IsWalking", isWalking);
+        
+
+
+
+        if (isWalking)
+        {
+            ForceMove(m_Rotation);
+            m_Movement.Normalize();
+
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+                
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop ();
+        }
+
+        
+       
+    }
+
+    void ForceRotation(ref Quaternion m_Rotation)
+    {
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+        //ForceMove();
+    }
+
+    void ForceMove(Quaternion m_Rotation)
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        transform.Translate(m_Movement * speed * Time.fixedDeltaTime);
+        ForceRotation(ref m_Rotation);
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+    }
+    
+
+    void OnAnimatorMove ()
+    {
+        m_Rigidbody.MovePosition (m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation (m_Rotation);
+        
+    }
+}
